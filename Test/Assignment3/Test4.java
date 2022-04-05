4549ba8585d3f838adbc2c30531dcdf7e0870935
undo <lhk2783497478@outlook.com>
2022-04-05 16:59:17 +0800

    change

--- Test/Assignment3/Test4.java
+++ Test/Assignment3/Test4.java
@@ -4,13 +4,36 @@ class Power{
     //set the variable
     int x=10,y=10;
     void ff2(int passX,int passY){
-        System.out.println("Initially,x="+x+",y="+y);
+        System.out.println("Before the process,x="+x+",y="+y);
         x=passX*passX;
         y=passY*passY;
+        System.out.println("In the process,x is:"+x+",y="+y);
+
     }
+}
+class Power2{
+    void ff2(data data1){
+        System.out.println("Before the process,data's x is:"+data1.x);
+        data1.x=100;
+        System.out.println("In the process,data's x is:"+data1.x);
+    }
+}
 
-
+class data{
+    int x=10;
+    data(int num){x=num;}
 }
 public class Test4 {
+    public static void main(String[] args) {
+        Power p=new Power();
+        p.ff2(100,100);
+        System.out.println("After the process x="+p.x+", y="+p.y);
+        System.out.println("===================================");
+        data data1=new data(10);
+        Power2 p2=new Power2();
+        p2.ff2(data1);
+        System.out.println("After the process,data's x is "+data1.x);
+
+    }
 
 }
---
+++ 基础学习/数据类型/StringLearning.java
@@ -0,0 +1,16 @@
+public class StringLearning {
+    public static void main(String args[]){
+        String s1=new String("java");
+        String s2=new String("java");
+        if(s1==s2){
+            System.out.println("s1==s2");
+        }else{
+            System.out.println("s1!=s2");
+        }
+        if(s1.equals(s2)){
+            System.out.println("s1.equals(s2)");
+        }else{
+            System.out.println("s1 not equals to s2");
+        }
+    }
+}
